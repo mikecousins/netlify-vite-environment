25b97a0e3fb45d750ca032314fa852d465cda3e5
Mike Cousins <mike@mikecousins.com>
2021-07-09 22:56:50 -0600

    Creating our sample repo

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p>Commit Ref: {import.meta.env.VITE_COMMIT_REF}</p>
+        <p>Netlify UI Variable: {import.meta.env.VITE_NETLIFY_CUSTOM}</p>
       </header>
     </div>
   )
